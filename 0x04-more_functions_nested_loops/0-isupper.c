7c26b565ae2e3a8a4a76f8c97af14f295b8ef225
Favour <favouropara48@gmail.com>
2023-03-23 04:52:50 -0700

    A

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<ctype.h>
+/**
+ * main - Print alphabets
+ * Return: 0
+ */
+
+int main(void)
+{
+	int c;
+
+	int _isupper(int c);
+	{
+		if (c >= 'A' && c <= 'Z')
+		{
+			return (1);
+		}
+		else
+		{
+			return (0);
+		}
+	}
+}
---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,7 @@
+#ifdef main_h
+#define main_h
+
+int _isupper(int c);
+int _isdigit(int c);
+
+#endif
