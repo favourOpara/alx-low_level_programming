422559298ed9eb800c793517c132be38285c5866
Favour <favouropara48@gmail.com>
2023-03-24 08:37:14 -0700

    A

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -11,7 +11,7 @@ void print_number(int n)
 	if (n < 0)
 	{
 		n1 = -n;
-		_putchar('-');
+		putchar('-');
 	}
 	else
 	{
@@ -21,5 +21,5 @@ void print_number(int n)
 	{
 		print_number(n1 / 10);
 	}
-	_putchar((n1 % 10) + '0');
+	putchar((n1 % 10) + '0');
 }
