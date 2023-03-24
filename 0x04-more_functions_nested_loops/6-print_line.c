4bc349e4ab5326dbc1a6305e239d3bca935c10df
Favour <favouropara48@gmail.com>
2023-03-24 07:24:58 -0700

    A

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_line - draws a straight line
+ * @n: number of times the charact
+ */
+void print_line(int n)
+{
+	if (n <= 0)
+	{
+		putchar('\n');
+	}
+	else
+	{
+		int i;
+
+		for (i = 1; i <= n; i++)
+		{
+			putchar('_');
+		}
+		putchar('\n');
+	}
+}
