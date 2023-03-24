fe0a95e8db1e1d7d01e5e7099b6ae419f854f66b
Favour <favouropara48@gmail.com>
2023-03-24 07:08:06 -0700

    A

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -14,10 +14,8 @@ void more_numbers(void)
 		for (j = 0; j < 15; j++)
 		{
 			if (j >= 10)
-			{
 				putchar(j / 10 + '0');
-				putchar(j % 10 + '0');
-			}
+			putchar(j % 10 + '0');
 		}
 		putchar('\n');
 	}
